4129f9e05e37feb26c1dcfe05476b1080f2ee151
FyFean <72227868+FyFean@users.noreply.github.com>
2022-05-24 18:55:10 +0200

    topaja

--- app/src/main/java/si/uni_lj/fe/tnuv/tnuv_app/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/tnuv_app/MainActivity.java
@@ -1,18 +1,53 @@
 package si.uni_lj.fe.tnuv.tnuv_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationBarView navigationBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationBarView = findViewById(R.id.bottom_nav);
+
+        navigationBarView.setOnItemSelectedListener(bottomNavMethod);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+
     }
+
+    private NavigationBarView.OnItemSelectedListener bottomNavMethod = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.profile:
+                    fragment = new HomeFragment();
+                case R.id.dumbbell:
+                    fragment = new WorkoutFragment();
+                case R.id.list:
+                    fragment = new ListFragment();
+
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
+
+
 
     //haha ballers, torej balls, jajcka even, testiculars if u prefer
 
